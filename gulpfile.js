621586a4828aa6814f1e05146d87404cb206f30d
Inoyatullo <you@example.com>
2020-03-22 18:17:59 +0500

    feat: add header and main and footer in index.html

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,40 @@
-const gulp = require ('gulp');
+const {src, dest, watch} = require ('gulp');
 const browserSync = require('browser-sync').create();
 const cssmin = require('gulp-cssmin');
 const rename = require('gulp-rename');
-
-gulp.task('hello', function(done) {
-  console.log('привет, мир!');
-  done();
-});
+const sass = require ('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 // Static server
-gulp.task('browser-sync', function() {
+function bs() {
+  serveSass();
   browserSync.init({
       server: {
           baseDir: "./"
       }
   });
-  gulp.watch("./*.html").on('change', browserSync.reload);
-});
+  watch("./*.html").on('change', browserSync.reload);
+  watch("./sass/**/*.sass", serveSass);
+  watch("./sass/**/*.scss", serveSass);
+  watch("./js/*.js").on('change', browserSync.reload);
+};
 
-gulp.task('replace', function (done) {
-  gulp.src('./css/**/*.css')
+function replace(done) {
+  return src('./css/**/*.css')
       .pipe(cssmin())
       .pipe(rename({suffix: '.min'}))
-      .pipe(gulp.dest('./css'));
+      .pipe(dest('./css'));
       done(); 
-});
+};
+
+function serveSass() {
+  return src("./sass/**/*.sass","./sass/**/*.scss")
+      .pipe(sass())
+      .pipe(autoprefixer({
+        cascade: false
+      }))
+      .pipe(dest("./css"))
+      .pipe(browserSync.stream());
+};
+
+exports.serve = bs;
---
+++ js/main.js
@@ -0,0 +1,194 @@
+/* document.addEventListener("DOMContentLoaded", (event) => {
+  
+  const modal = document.querySelector(".modal"),
+      switchModal = () => modal.classList.toggle('modal--visible');
+
+
+  document.body.addEventListener('click', ev => {
+      const {target} = ev;
+      if (target.matches('[data-toggle=modal], .modal__close')) {
+          switchModal();
+      } else if (!target.closest('.modal') && modal.classList.contains('modal--visible')) {
+          switchModal();
+      }
+  });
+
+  document.addEventListener('keydown', e => {
+    e.keyCode === 27 && modal.classList.contains('modal--visible') ? switchModal() : false;
+});
+}); */
+
+$(document).ready(function () {
+  var modal = $('.modal'),
+  modalBtn = $('[data-toggle="modal"]');
+  closeBtn = $('.modal__close');
+
+  modalBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+  closeBtn.on('click', function () {
+    modal.toggleClass('modal--visible');
+  });
+
+
+  var mySwiper = new Swiper ('.swiper-container', {
+    loop: true,
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  })
+
+  var next = $('.swiper-button-next');
+  var prev = $('.swiper-button-prev');
+  var bullets = $('.swiper-pagination');
+
+  next.css('left', prev.width() +15 +bullets.width() +15)
+  bullets.css('left', prev.width() +15)
+
+  new WOW().init();
+
+  $('.modal__form').validate({
+    errorElement: "div",
+    errorClass: "invalid",
+    rules: {
+      // simple rule, converted to {required:true}
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      // compound rule
+      userEmail: {
+        required: true,
+        email: true
+      },
+      policyCheckbox: "required"
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух символов",
+        maxlength: "Имя не больше 15 символов"
+      },
+      userPhone: "Номер телефона обязательно",
+      userEmail: {
+        required: " указать адрес электронной почты",
+        email: "Введите в фомате: name@domain.com"
+      }
+    }
+
+  });
+
+  $('.control__form').validate({
+    errorElement: "em",
+    errorClass: "invalid",
+    rules: {
+      // simple rule, converted to {required:true}
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      policyCheckbox: "required"
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух символов",
+        maxlength: "Имя не больше 15 символов"
+      },
+      userPhone: "Номер телефона обязательно",
+      policyCheckbox: ""
+    }
+
+  });
+
+  $('.footer__form').validate({
+    errorElement: "div",
+    errorClass: "invalid",
+    rules: {
+      // simple rule, converted to {required:true}
+      userName: {
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      },
+      userPhone: "required",
+      // compound rule
+      userQuestion: "required",
+      policyCheckbox: "required"
+    },
+    messages: {
+      userName: {
+        required: "Имя обязательно",
+        minlength: "Имя не короче двух символов",
+        maxlength: "Имя не больше 15 символов"
+      },
+      userPhone: "Номер телефона обязательно",
+      userQuestion: {
+        required: " задать вопрос",
+      }
+    }
+
+  });
+
+  // маска для телефона
+  $('[type=tel]').mask('+7(000) 00-00-000', {placeholder: "+7(___) __-__-___"}) 
+
+  // Функция ymaps.ready() будет вызвана, когда
+
+  ymaps.ready(function () {
+    var myMap = new ymaps.Map('map', {
+            center: [55.750783, 37.592578],
+            zoom: 9
+        }, {
+            searchControlProvider: 'yandex#search'
+        }),
+
+        // Создаём макет содержимого.
+        MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+            '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+        ),
+
+        myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'Наш офис',
+            balloonContent: 'Вход со двора'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/marker.png',
+            // Размеры метки.
+            iconImageSize: [32, 32],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-5, -38]
+        });
+
+    myMap.geoObjects
+        .add(myPlacemark);
+});
+
+});
+
+/* $(document).ready(function () {
+  var btnTop = $('.btn-top');
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() >= 350 ) {
+      $(btnTop).fadeIn();
+    }else {
+      $(btnTop).fadeOut();
+    }
+  });
+  $(btnTop).on('click', function () {
+    $('html,body').animate({scrollTop:0}, 1000);
+  });
+}); */
